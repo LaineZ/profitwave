7263a817ea53194a97b6f5369c4b932b2586acc6
LaineZ <laineprikol@gmail.com>
2023-06-14 21:59:29 +0300

    Layout fixes for Desktop

--- src/app/main-slide/main-slide.component.ts
+++ src/app/main-slide/main-slide.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-slide.component.scss']
 })
 export class MainSlideComponent {
-  imageRocketTop = 0;
-  imageBallsTop = 0;
-  imageRocketRight = 0;
+  imageBallsX = 0;
+  imageBallsY = 0;
+  imageRocketY = 0;
 
   onMouseMove(event: MouseEvent) {
-    this.imageRocketTop = event.clientY * 0.01;
-    this.imageRocketRight = event.clientX * 0.01;
-    this.imageBallsTop = event.clientY * -0.02;
+    this.imageRocketY = event.clientY * 0.01;
+    this.imageBallsX = event.clientX * -0.01;
+    this.imageBallsY = event.clientY * -0.01;
   }
 }
