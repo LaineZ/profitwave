9065d69b09258c8269c51069afbba6279018296c
LaineZ <laineprikol@gmail.com>
2023-06-17 23:08:57 +0300

    Subscribe parallax

--- src/app/slide-subscribe/slide-subscribe.component.ts
+++ src/app/slide-subscribe/slide-subscribe.component.ts
@@ -6,11 +6,12 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./slide-subscribe.component.scss']
 })
 export class SlideSubscribeComponent {
-  public backgroundPosition: string = '0% 5%, 100% 100%';
-  private scrollSpeed: number = 10;
+  imageX = 0;
+  imageY = 0;
 
-  @HostListener('window:scroll', ['$event']) onScroll(event: Event) {
-    const scrollPosition = window.scrollY;
-    this.backgroundPosition = `0% calc(-60% + ${scrollPosition / this.scrollSpeed}px), 100% calc(20% + ${scrollPosition / this.scrollSpeed}px)`;
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.imageY = event.clientY * 0.05;
+    this.imageX = event.clientX * -0.05;
   }
 }
