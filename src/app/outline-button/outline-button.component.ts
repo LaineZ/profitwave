703032f3607a561313fc4cf1066bde668424c025
LaineZ <laineprikol@gmail.com>
2023-06-17 22:39:14 +0300

    Fixed multiple adoptations

--- src/app/outline-button/outline-button.component.ts
+++ src/app/outline-button/outline-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-outline-button',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./outline-button.component.scss']
 })
 export class OutlineButtonComponent {
-
+  @Input() href: string = "";
 }
